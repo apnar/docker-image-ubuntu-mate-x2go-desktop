76819caf32c4e387130b791b7bc0923441379f86
apnar <jlukens@botch.com>
2017-03-17 17:10:19 -0400

    directly exec supervisord

--- ubuntu-base/docker-entrypoint.sh
+++ ubuntu-base/docker-entrypoint.sh
@@ -3,5 +3,4 @@ if [ ! -f /.root_pw_set ]; then
 	/set_root_pw.sh
 fi
 
-#exec $@
-/usr/bin/supervisord -n -c /etc/supervisor/supervisord.conf 1>/dev/null
+exec /usr/bin/supervisord -n -c /etc/supervisor/supervisord.conf 1>/dev/null
